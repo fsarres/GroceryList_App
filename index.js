a4f648ad4fce68f1ad3e0836d892151b41367813
Felipe Sarres <sarres@gmail.com>
2023-04-14 14:56:26 -0500

    added styling

---
+++ index.js
@@ -0,0 +1,54 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL: "https://grocery-list-7a755-default-rtdb.firebaseio.com/"
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
+const inputFieldEl = document.getElementById('input-field');
+const addButtonEl = document.getElementById('add-button');
+
+const shoppingListEl = document.getElementById('shopping-list');
+
+addButtonEl.addEventListener('click', function () {
+  let inputValue = inputFieldEl.value;
+
+  push(shoppingListInDB, inputValue);
+
+  clearInputFieldEl();
+
+});
+
+onValue(shoppingListInDB, function (snapshot) {
+  let itemsArray = Object.entries(snapshot.val());
+  clearShoppingListEl();
+
+  for (let i = 0; i < itemsArray.length; i++) {
+    let currentItem = itemsArray[i];
+
+    let currentItemId = currentItem[0];
+    let currentItemValue = currentItem[1];
+
+    appendToListEl(currentItemValue);
+  }
+});
+
+function clearShoppingListEl() {
+  shoppingListEl.innerHTML = '';
+}
+
+function clearInputFieldEl() {
+  inputFieldEl.value = '';
+}
+
+function appendToListEl(itemValue) {
+  //shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+
+  let newEl = document.createElement('li');
+
+}
